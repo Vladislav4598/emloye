e158f0f5ab88fcd2bec17627ea5dc8643ca583c7
Vladislav4598 <39894505+Vladislav4598@users.noreply.github.com>
2021-09-27 04:18:02 +0700

    Add files via upload
    файлы

--- date.php
+++ date.php
@@ -4,8 +4,11 @@
 
 	$id_v= $_POST['id_v'];
 
+
+
+
 	if($id_v == 1){
-		$sql = 'select * from user, user_position, department, position where user.id = user_position.user_id and user_position.department_id = department.id and user_position.position_id = position.id order by user.last_name ';
+		$sql = 'select * from user, user_position, department, position, boss where user.id = user_position.user_id and user_position.department_id = department.id and user_position.position_id = position.id and user.id <> boss.boss_id and user.id = boss.user_id and ((DATEDIFF(date_dismission, date_of_receipt)) < 90) order by user.last_name  ';
 		$query = mysqli_query($con, $sql);
 
 		while ($row = mysqli_fetch_array($query)) {
@@ -29,12 +32,18 @@
 
 
 
-
-
-
 	}
 
 	if($id_v == 3){
+		$sql = 'select * from user, user_position, department, position, boss,(SELECT boss.boss_id ,MIN(DATEDIFF(CURDATE(), date_of_receipt)) as date2 from boss,user where user.id = boss.user_id and user.id <> boss.boss_id GROUP by boss.boss_id ) a where user.id = user_position.user_id and user_position.department_id = department.id and user_position.position_id = position.id and user.id <> boss.boss_id and user.id = boss.user_id and (DATEDIFF(CURDATE(), date_of_receipt)) = date2 order by description_department ';
+		$query = mysqli_query($con, $sql);
+
+		while ($row = mysqli_fetch_array($query)) {
+
+			$output[] = $row;
+
+		}
+		echo json_encode($output, JSON_UNESCAPED_UNICODE);
 
 
 
@@ -42,6 +51,7 @@
 
 
 	}
+
 
 
 
--- index.php
+++ index.php
@@ -15,11 +15,13 @@
 	?>
 
 		<div class = "conten">
-
-
+			<form action="index.php">
+				<input type="submit" name="fggdg"  value = "Обновить">
+			</form>
 			<input type="submit" name="rgd" onclick="Output(1)" value = "Испытательный срок">
 			<input type="submit" name="dfg" onclick="Output(2)" value = "Уволенные">
 			<input type="submit" name="fdg" onclick="Output(3)" value = "Начальники">
+
 		</div>
 
 
@@ -27,15 +29,53 @@
 
 
 
+
 <table id = table border="1" align="center" width="100%">
+	<tr>
+		<th>№</th>
+		<th>ФИО</th>
+		<th>Дата рождения</th>
+		<th>Должность</th>
+		<th>Размер заработной платы</th>
+		<th>Дата приема на работ</th>
+		<th>Дата увольнения</th>
+		<th>Причина увольнения</th>
+		<th>Начальник</th>
+	</tr>
+	<?php
+	for ($i=0; $i < 20; $i++) {
+		 echo '<tr>
+		<th>-</th>
+		<th>-</th>
+		<th>-</th>
+		<th>-</th>
+		<th>-</th>
+		<th>-</th>
+		<th>-</th>
+		<th>-</th>
+		<th>-</th>
+	</tr>' ;
+	}
+
+
+
+	?>
 
 
 </table>
 
+
+
+
+
 <script type="text/javascript">
 
 
+
+
 	function Output(id_v){
+
+
 
      	 $.ajax({
 	              method: "POST",
@@ -45,9 +85,13 @@
 	              data: { id_v: id_v }
 	            })
 	              .done(function( data ) {
-	              	///alert(data);
+
+	              	//console.log(data);
+
 
 	              	var user = $.parseJSON(data);
+
+	              	console.log(user);
 
 	              	$('#table').empty().append(
 		              	$('<tr>').append(
@@ -55,32 +99,32 @@
 		              	$('<th>').text('ФИО'),
 		              	$('<th>').text('Дата рождения'),
 		              	$('<th>').text('Должность'),
-		              	$('<th>').text('Дата приема на работу'),
-		              	$('<th>').text('Дата увольнения'),
-		              	$('<th>').text('Причина увольнения'),
 		              	$('<th>').text('Размер заработной платы'),
+		              	$('<th>').text('Дата приема на работу'),
+		              	id_v != 3 ?
+		              	$('<th>').text('Дата увольнения') :
+		              	id_v == 2 ? $('<th>').text('Причина увольнения') :
 		              	$('<th>').text('Начальник'),
 
 
-
-		              	)
+		             	)
 		              	);
 
-	              	$('#fio').text('ФИО');
 
 	              	$('#table').append(...user.map((el)=>(
 
-
 	              		$('<tr>').append(
-	              		$('<th>').text(el.id ),
+	              		$('<th>').text(el.user_id),
                       	$('<th>').text(el.last_name + ' ' + el.first_name +  ' ' + el.middle_name ),
-                      	$('<th>').text(el.data_of_birth),
+                      	$('<th>').text((el.data_of_birth).split("-").reverse().join("-")),
                       	$('<th>').text(el.name_department + ' ( ' + el.description_department +') ' + ' - ' + el.name_position),
-                      	$('<th>').text(el.created_at),
-                      	$('<th>').text(el.updated_at),
-                      	//$('<th>').text(el.description_dismission_reason),
-                      	$('<th>').text(el.salary),
-                      	$('<th>').text('-'),
+						$('<th>').text(el.salary + ' руб.'),
+						$('<th>').text((el.date_of_receipt).split("-").reverse().join("-")),
+						id_v != 3 ?
+						$('<th>').text((el.date_dismission).split("-").reverse().join("-")) :
+
+                      	id_v == 2 ? $('<th>').text(el.description_dismission_reason):
+						$('<th>').text(el.description_department),
 
                       	)
                       	)));
